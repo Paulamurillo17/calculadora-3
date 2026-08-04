887e0b6f2d43bb0fd587e26d333283636d66b208
ylpjk <Yazamorama@est.utn.ac.cr>
2026-08-04 15:31:37 +0800

    Se realizaron dos clase mas
     Se realizaon dos clase mas y se modifico la clase Calculadora

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -9,5 +9,68 @@ package calculadora;
  * @author ylpjk
  */
 public class Calculadora {
-    
+ 
+    private double valor;
+    private double resultado;
+    private String operador;
+
+    public Calculadora() {
+        resultado = 0;
+    }
+
+    public void sumar() {
+        resultado += valor;
+    }
+
+    public void restar() {
+        resultado -= valor;
+    }
+
+    public void multiplicar() {
+        resultado *= valor;
+    }
+
+    public void dividir() throws DivisionPorCero, OperacionInvalidad {
+        if (valor == 0) {
+            throw new DivisionPorCero("No se puede dividir entre cero");
+        }
+        resultado /= valor;
+        if (Double.isNaN(resultado) || Double.isInfinite(resultado)) {
+            throw new OperacionInvalidad("El resultado no es válido");
+        }
+    }
+
+    public void reiniciar() {
+        resultado = 0;
+        valor = 0;
+        operador = null;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
+
+    public String getOperador() {
+        return operador;
+    }
+
+    public void setOperador(String operador) {
+        this.operador = operador;
+    }
 }
+
+
+
+
---
+++ src/calculadora/DivisionPorCero.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author ylpjk
+ */
+public class DivisionPorCero extends Exception {
+    
+public DivisionPorCero(String mensaje){
+    super(mensaje);
+}
+
+}
